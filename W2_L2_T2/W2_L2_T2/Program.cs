d1393eb211090a2b6bc5ffaa26c341918e152049
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-19 21:49:09 +0200

    Task 2 finished

---
+++ W2_L2_T2/W2_L2_T2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace W2_L2_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = 'A';
+            char char2 = 'B';
+            char char3 = 'C';
+
+            Console.WriteLine($"Zmienna 1: {char3}\nZmienna 2: {char2}\nZmienna 3: {char1}");
+        }
+    }
+}
