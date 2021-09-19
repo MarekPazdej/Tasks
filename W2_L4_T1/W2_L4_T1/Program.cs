694a7386d5aced564e544536fc1f3c05b9da125b
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-19 21:37:14 +0200

    Task Finished

---
+++ W2_L4_T1/W2_L4_T1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace W2_L4_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string lastName;
+            int age;
+            char gender = 'm';
+            string pesel;
+            string employeeNumber;
+        }
+    }
+}
