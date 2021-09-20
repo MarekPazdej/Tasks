161da618fbbb32f6187393d4b7a2f302d289fa20
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-20 21:26:23 +0200

    Task 3, done.

---
+++ W2_L7_T3/W2_L7_T3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W2_L7_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (userNumber > 0)
+            {
+                Console.WriteLine($"{userNumber} jest liczbą dodatnią.");
+            }
+            else
+            {
+                Console.WriteLine($"{userNumber} jest liczbą ujemną.");
+            }
+        }
+    }
+}
