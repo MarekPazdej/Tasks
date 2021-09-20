dd5d13315cec86b8c85744d6e671646c9cb1ac70
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-20 21:06:01 +0200

    Task 2, done

---
+++ W2_L7_T2/W2_L7_T2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace W2_L7_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter number.");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine($"{userNumber} jest liczbą parzystą.");
+            }
+            else
+            {
+                Console.WriteLine($"{userNumber} nie jest parzystą.");
+            }
+        }
+    }
+}
