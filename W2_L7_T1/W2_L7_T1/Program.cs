0212146c27c2c645ebe75c34eeb1504912ef17f0
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-20 20:52:39 +0200

    Done

---
+++ W2_L7_T1/W2_L7_T1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W2_L7_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int number2 = 5;
+
+            if (number == number2)
+            {
+                Console.WriteLine($"{number} i {number2} są równe");
+            }
+            else
+            {
+                Console.WriteLine("Liczby nie są równe.");
+            }
+        }
+    }
+}
