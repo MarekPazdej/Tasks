7cd6811945f0b38ef58d3109eeeac0aee0ff3f59
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-20 15:13:49 +0200

    Task finished.

---
+++ W2_L4_T4/W2_L4_T4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace W2_L4_T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            double number1 = 12.5;
+            string name = "Szkoła Dotneta";
+        }
+    }
+}
