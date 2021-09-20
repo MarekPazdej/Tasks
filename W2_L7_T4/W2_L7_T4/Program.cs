d821c117ed15283c5bbf88396dfa024a192e30c7
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-20 21:50:35 +0200

    Task 4, done.

---
+++ W2_L7_T4/W2_L7_T4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace W2_L7_T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok do sprawdzenia.");
+
+            int yearToCheck = Convert.ToInt32(Console.ReadLine());
+
+            if((yearToCheck % 4 == 0 && yearToCheck % 100 != 0) || yearToCheck % 400 == 0)
+            {
+                Console.WriteLine($"{yearToCheck} jest rokiem przestępnym.");
+            }
+            else
+            {
+                Console.WriteLine($"{yearToCheck} nie jest rokiem przestępnym.");
+            }
+        }
+    }
+}
