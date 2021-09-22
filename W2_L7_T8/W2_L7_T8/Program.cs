a7f30bb2622785256ba696c11f1a2577befe2dc1
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-22 21:56:23 +0200

    Task 8, done.

---
+++ W2_L7_T8/W2_L7_T8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace W2_L7_T8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte sumOfResults = 0;
+
+            Console.WriteLine("Podaj wynik z matematyki:");
+            byte mathsResult = Convert.ToByte(Console.ReadLine());
+            sumOfResults = mathsResult;
+
+            Console.WriteLine("Podaj Wynik z fizyki:");
+            byte physicsResult = Convert.ToByte(Console.ReadLine());
+            sumOfResults += physicsResult;
+
+            Console.WriteLine("Podaj wynik z chemi:");
+            byte chemistryResult = Convert.ToByte(Console.ReadLine());
+            sumOfResults += chemistryResult;
+
+            Console.WriteLine(sumOfResults);
+
+            if ((mathsResult >= 70 && physicsResult >= 45 && chemistryResult >= 45 && sumOfResults >= 180) || mathsResult + physicsResult >= 150 || mathsResult + chemistryResult >= 150)
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji.");
+            }
+            else
+            {
+                Console.WriteLine("Kandydat nie dopuszczony do rekrutacji.");
+            }
+        }
+    }
+}
