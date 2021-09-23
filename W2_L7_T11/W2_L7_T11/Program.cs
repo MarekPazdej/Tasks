6b3d6e58d8d8f81986fa0e6a41d6ffa26bd3d5b6
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-23 18:41:03 +0200

    Task 11, done.

---
+++ W2_L7_T11/W2_L7_T11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace W2_L7_T11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ocenę w postaci liczby.");
+            sbyte rating = Convert.ToSByte(Console.ReadLine());
+
+            switch (rating)
+            {
+                case 1:
+                    Console.WriteLine("Niedostateczny");
+                    break;
+                case 2:
+                    Console.WriteLine("Dopuszczający");
+                    break;
+                case 3:
+                    Console.WriteLine("Dostateczny");
+                    break;
+                case 4:
+                    Console.WriteLine("Dobry");
+                    break;
+                case 5:
+                    Console.WriteLine("Bardzo dobry");
+                    break;
+                case 6:
+                    Console.WriteLine("Celujący");
+                    break;
+                default:
+                    Console.WriteLine("To nie jest ocena.");
+                    break;
+            }
+        }
+    }
+}
