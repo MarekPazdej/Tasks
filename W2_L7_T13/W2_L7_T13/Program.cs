e351438092ac3ba21341729192f8fd55cbddfccf
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-23 22:34:36 +0200

    Task 13, done.

---
+++ W2_L7_T13/W2_L7_T13/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace W2_L7_T13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prosty kalkulator\n");
+
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Podaj drugą liczbę:");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("\nPodaj numer operacji do wykonania:\n");
+            Console.WriteLine("\t1. Dodawanie");
+            Console.WriteLine("\t2. Odejmowanie");
+            Console.WriteLine("\t3. Mnożenie");
+            Console.WriteLine("\t4. Dzielenie");
+
+            sbyte userChoice = Convert.ToSByte(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    Console.WriteLine($"\nTwój wynik to: " + (firstNumber + secondNumber));
+                    break;
+                case 2:
+                    Console.WriteLine("\ntwój wynik to: " + (firstNumber - secondNumber));
+                    break;
+                case 3:
+                    Console.WriteLine("\nTwój wynik to: " + (firstNumber * secondNumber));
+                    break;
+                case 4:
+                    if (secondNumber != 0)
+                    {
+                        Console.WriteLine("\nTwój wynik to: " + (firstNumber / secondNumber));
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nPamiętaj... nie dziel przez ZERO.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("\nNie ma takiej opcji.");
+                    break;
+            }
+        }
+    }
+}
