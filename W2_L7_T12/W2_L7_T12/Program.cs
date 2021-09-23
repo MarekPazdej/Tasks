88febe36dbfb4fa5f38039fa4b807c54169ec7dd
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-23 21:19:04 +0200

    Task 12, done.

---
+++ W2_L7_T12/W2_L7_T12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace W2_L7_T12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj numer dnia tygodnia od 1 do 7.");
+            sbyte daynumber = Convert.ToSByte(Console.ReadLine());
+
+            switch (daynumber)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+                case 6:
+                    Console.WriteLine("Sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("Niedziela");
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiego dnia tygodnia.");
+                    break;
+            }
+        }
+    }
+}
