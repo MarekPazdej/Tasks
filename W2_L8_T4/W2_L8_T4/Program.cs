3043aadccef38171a07d8690cde8e62e837a15da
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-24 22:43:41 +0200

    Task 4, done.

---
+++ W2_L8_T4/W2_L8_T4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace W2_L8_T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int counter = 1;
+            //bool stop = false;
+
+            Console.WriteLine();
+            for (int i = 1; i < userNumber; i++)
+            {
+                if (counter <= userNumber)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (counter <= userNumber)
+                        {
+                            Console.Write(counter + " ");
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
