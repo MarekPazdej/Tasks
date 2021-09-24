5dbe1840f474289770f40f5032360e3a1d0e7ed8
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-24 16:34:54 +0200

    Task 3, done.

---
+++ W2_L8_T3/W2_L8_T3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace W2_L8_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibA = 0;
+            int fibB = 1;
+            int fibVolume = 0;
+
+            Console.WriteLine(fibA);
+            Console.WriteLine(fibB);
+            do
+            {
+                fibVolume = fibA + fibB;
+                Console.WriteLine(fibVolume);
+                fibA = fibB;
+                fibB = fibVolume;
+            } while (fibVolume <= 1000000);
+        }
+    }
+}
