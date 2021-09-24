833b4bbb8a7e0fdc2636694e826ad5da73e8940f
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-24 15:23:57 +0200

    Task 1, done.

---
+++ W2_L8_T1/W2_L8_T1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W2_L8_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                if ((i % 2 == 0 && i / 2 == 1) || (i % 3 == 0 && i / 3 == 1) || (i % 5 == 0 && i / 5 == 1) || (i % 7 == 0 && i / 7 == 1))
+                {
+                    Console.Write(i + " ,");
+                }
+                else if (i % 2 >= 1 && i % 3 >= 1 && i % 5 >= 1 && i % 7 >= 1)
+                {
+                    Console.Write(i + " ,");
+                }
+            }
+        }
+    }
+}
