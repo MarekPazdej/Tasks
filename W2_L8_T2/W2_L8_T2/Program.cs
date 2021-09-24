8567e11b685eb14ab24301ec8f33d751201c090a
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-24 15:56:14 +0200

    Task 2, done.

---
+++ W2_L8_T2/W2_L8_T2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace W2_L8_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ,");
+                }
+                i++;
+            } while (i <= 1000);
+            Console.WriteLine();
+        }
+    }
+}
