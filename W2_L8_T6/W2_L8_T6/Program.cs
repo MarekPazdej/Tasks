7da359116351fe10f10283258b33024dd766ec29
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-25 20:49:21 +0200

    Task 6, still to be finished.

---
+++ W2_L8_T6/W2_L8_T6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace W2_L8_T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //czekam na odpowiedź z forum
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
