1b1e4ddfb28560ed96aae31981f57a6a4464d4a1
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-25 20:30:19 +0200

    Task 5, done.

---
+++ W2_L8_T5/W2_L8_T5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace W2_L8_T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                double result = Math.Pow(i, 3);
+                Console.WriteLine($"{i} do potęgi trzeciej to {result}");
+            }
+        }
+    }
+}
