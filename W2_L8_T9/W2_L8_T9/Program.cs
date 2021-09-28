a6c72a96b5f4a453f9ec1d0ed187f70e765a434a
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-28 22:14:29 +0200

    Not finished yet.

---
+++ W2_L8_T9/W2_L8_T9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W2_L8_T9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
