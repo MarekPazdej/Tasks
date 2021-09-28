bbcf7634d99c2bc0cbc74c28186dbce7e901eb42
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-28 21:24:25 +0200

    Task 7, done.

---
+++ W2_L8_T7/W2_L8_T7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace W2_L8_T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość boku diamentu: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            char whiteSpaces = ' ';
+            char printedSings = '*';
+            int rows = 0;
+            int columns = 0;
+            int spaces = 0;
+
+            while (rows<userNumber)
+            {
+                for (columns = 0; columns < userNumber - spaces; columns++)
+                {
+                    Console.Write(whiteSpaces);
+                }
+                for (columns = 0; columns <= rows+spaces; columns++)
+                {
+                    Console.Write(printedSings);
+                }
+                Console.WriteLine();
+                rows++;
+                spaces++;
+            }
+
+
+            rows--;
+            spaces--;
+            while (rows >= 0)
+            {
+                for (columns = 0; columns <= userNumber - spaces; columns++)
+                {
+                    Console.Write(whiteSpaces);
+                }
+                for (columns = 0; columns < (rows+spaces)-1; columns++)
+                {
+                    Console.Write(printedSings);
+                }
+                Console.WriteLine();
+                rows--;
+                spaces--;
+            }
+
+        }
+    }
+}
