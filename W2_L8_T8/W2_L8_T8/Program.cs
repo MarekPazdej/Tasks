9855b96314a1810085b5fb6d3695297ef16e8263
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-28 22:10:30 +0200

    Task 8, done

---
+++ W2_L8_T8/W2_L8_T8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace W2_L8_T8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj przykładowy tekst:");
+            string userInput = Console.ReadLine();
+            string reversUserInput = "";
+            int stringLenght;
+
+            stringLenght = userInput.Length -1;
+
+            while (stringLenght >= 0)
+            {
+                reversUserInput = reversUserInput + userInput[stringLenght];
+                stringLenght--;
+            }
+            Console.WriteLine(reversUserInput);
+        }
+    }
+}
