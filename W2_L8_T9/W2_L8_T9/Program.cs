d730e1388e7a8deb68ab87078b3f80d4fd9777cf
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-29 13:33:21 +0200

    Task 9, done.

--- W2_L8_T9/W2_L8_T9/Program.cs
+++ W2_L8_T9/W2_L8_T9/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace W2_L8_T9
 {
@@ -6,7 +7,34 @@ namespace W2_L8_T9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberFromUser;
+            int dividedNumber;
+            int restOfDivision;
+            string binaryNumber = null;
+            string tempBinaryNumber;
+            string binaryNumberAfterReverse = null;
+            
+            Console.WriteLine("Podaj liczbę:\n");
+            numberFromUser = Convert.ToInt32(Console.ReadLine());
+            dividedNumber = numberFromUser;
+
+            while (dividedNumber > 0)
+            {
+                restOfDivision =  dividedNumber % 2;
+                tempBinaryNumber = Convert.ToString(restOfDivision);
+                binaryNumber += tempBinaryNumber;
+                dividedNumber = dividedNumber / 2;
+            }
+
+            int binNumberLenght = binaryNumber.Length - 1;
+            while (binNumberLenght >=0)
+            {
+                binaryNumberAfterReverse = binaryNumberAfterReverse + binaryNumber[binNumberLenght];
+                binNumberLenght--;
+            }
+            Console.WriteLine($"\nLiczba {numberFromUser} w systemie binarnym to {binaryNumberAfterReverse}");
+
+            
         }
     }
 }
