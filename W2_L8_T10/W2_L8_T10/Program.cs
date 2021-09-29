1c0477d957ceeab93e215539a73bccf7d1fe6c5b
MarekPazdej <72928831+MarekPazdej@users.noreply.github.com>
2021-09-29 22:24:11 +0200

    Task 10, done.

---
+++ W2_L8_T10/W2_L8_T10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace W2_L8_T10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int nwd;
+            int firstNwwNumber = firstNumber;
+            int secondNwwNumber = secondNumber;
+            int nww;
+
+            while (firstNumber != secondNumber)
+            {
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber -= secondNumber;
+                }
+                else
+                {
+                    secondNumber -= firstNumber;
+                }
+            }
+
+            nwd = firstNumber;
+            Console.WriteLine($"\nNWD wynosi: {nwd}");
+            nww = firstNwwNumber * secondNwwNumber / nwd;
+            Console.WriteLine($"\nNWW wynosi: {nww}");
+        }
+    }
+}
